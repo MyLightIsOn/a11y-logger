8055d6e3e57c6e48c36c01974dec014a559ac23f
Lawrence Moore <thelawrencemoore@gmail.com>
2025-03-05 05:29:17 -0800

    Add file upload capability to the issue edit page
    Introduced a new `AddIssueImagesForm` component for handling file uploads and integrated it into the issue edit page. This allows users to select and upload multiple files alongside editing issue details.

--- app/assessments/[documentId]/edit-issue/page.tsx
+++ app/assessments/[documentId]/edit-issue/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { AddIssueForm } from "@/components/forms/add-issue-form";
-import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 import { EditIssueForm } from "@/components/forms/edit-issue-form";
+import AddIssueImagesForm from "@/components/forms/add-issue-images-form";
 
 export default async function AddIssueRoute(props) {
   return (
     <div className="p-4">
+      <AddIssueImagesForm />
       <EditIssueForm />
     </div>
   );
